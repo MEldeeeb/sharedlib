ae42bf98c20b1b7c956a6922f52723f52fee62ff
Mohamed Eldeeb <87948077+MEldeeeb@users.noreply.github.com>
2025-05-26 22:24:09 +0300

    l ISA

--- vars/call_python_app.groovy
+++ vars/call_python_app.groovy
@@ -1,29 +1,29 @@
-
 def call() {
-    def dockerx = new org.iti.docker(this)
-    pipeline{
-        agent{
+    pipeline {
+        agent {
             label "bash"
         }
+
         environment {
             DOCKER_CREDS = credentials('dockerhub-user')
         }
 
-        stages{
-            stage("build Docker image using python"){
-                steps{
-                    script{
+        stages {
+            stage("build Docker image using python") {
+                steps {
+                    script {
+                        def dockerx = new org.iti.docker(this)
                         dockerx.build("meldeeeb/python-app", "${BUILD_NUMBER}")
                     }
-                
                 }
             }
-            stage("push Docker image using python"){
-                steps{
-                    script{
+
+            stage("push Docker image using python") {
+                steps {
+                    script {
+                        def dockerx = new org.iti.docker(this)
                         dockerx.login(env.DOCKER_CREDS_USR, env.DOCKER_CREDS_PSW)
                         dockerx.push("meldeeeb/python-app", "${BUILD_NUMBER}")
-
                     }
                 }
             }
